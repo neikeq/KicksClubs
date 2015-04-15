826170633a6415858ef8969ea351b8a2bdc5f27c
neikeq <ignalfonsore@gmail.com>
2015-04-15 13:24:49 +0200

    Added some comments in ClubsController

--- src/Neikeq/ClubsBundle/Controller/ClubsController.php
+++ src/Neikeq/ClubsBundle/Controller/ClubsController.php
@@ -17,29 +17,32 @@ class ClubsController extends Controller
     {
         $em = $this->get('doctrine.orm.entity_manager');
 
+        // get the number of clubs registered
         $clubsCountQB = $em->createQueryBuilder();
         $clubsCountQB->select('count(c.id)')
             ->from('NeikeqClubsBundle:Clubs','c');
         $resultAllClubs = $clubsCountQB->getQuery()->getSingleScalarResult();
 
+        // get a result of clubs for the current page
         $pageClubsQB = $em->createQueryBuilder();
         $pageClubsQB->select('c.id, c.name, c.creation')
            ->from('NeikeqClubsBundle:Clubs','c')
            ->setFirstResult(($page - 1) * self::clubsPerPage)
            ->setMaxResults(self::clubsPerPage);
-
         $results = $pageClubsQB->getQuery()->getResult();
 
+        // retrieve the clubs information from the result
         $clubs = array();
-
         foreach ($results as $club) {
             array_push($clubs, array("id" => $club['id'], "name" => $club['name'],
                 "creation" => $club['creation'], "manager" => "", "members" => ""));
         }
 
+        // if user is not authenticated, set username to empty
         $user = $this->getUser();
         $username = $user == null ? '' : $user->getUsername();
 
+        // params for the twig template
         $twigParams = array('username' => $username, 'clubs' => $clubs,
             'pages' => ceil($resultAllClubs / self::clubsPerPage));
 
